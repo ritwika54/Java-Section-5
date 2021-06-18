67ffc9b56c41d1cb845eeec7cb2938ce0c9d780f
ritwika54 <58146900+ritwika54@users.noreply.github.com>
2021-06-18 20:56:23 +0530

    Create Palindrome.java

---
+++ Palindrome.java
@@ -0,0 +1,23 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(1223));
+        System.out.println(isPalindrome(1441));
+    }
+
+    public static boolean isPalindrome(int number) {
+
+        int startNumber = number;
+        int reverseVal = 0;
+        int lastNumber;
+
+        while(number != 0) {
+
+            lastNumber = number % 10;
+            reverseVal *= 10;
+            reverseVal += lastNumber;
+            number /= 10;
+
+        }
+        return reverseVal == startNumber;
+    }
+}
